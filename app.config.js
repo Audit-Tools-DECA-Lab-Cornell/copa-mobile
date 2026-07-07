9139c304df55ac46ff4fc868deab026fe0f894e4
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-07-06 22:45:07 -0400

    chore(release): v0.6.4

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
     expo: {
         name: "COPA",
         slug: "audit-tools-playspace-mobile",
-        version: "0.6.3",
+        version: "0.6.4",
         orientation: "portrait",
         scheme: "copa-mobile",
         icon: "./assets/icon.png",
